0c1d519b59219742d4bf4ea76fdb7e474f2eef4d
Thomas Boerger <tboerger@tbpro.de>
2013-12-21 16:47:10 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name "imagemagick"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures imagemagick"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "imagemagick", "Installs/Configures imagemagick"
+
+supports "suse", ">= 12.3"
+supports "debian", ">= 7.0"
+supports "ubuntu", ">= 12.04"
